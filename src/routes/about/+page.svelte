d05f13b18e4d51680010b6787f676f4e5b3a03b7
Ashley Peterson <nosretepea@gmail.com>
2023-12-29 22:00:22 -0500

    Initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<div style="display: flex; flex-direction: column; align-items: center;">
+	<nav>
+		<a href="/">home</a>
+		<a href="/about">about</a>
+		<a href="/photos">photos</a>
+	</nav>
+</div>
+
+<div
+	style="display: flex; flex-direction: column; flex-grow: 1; justify-content: center; text-align: center;"
+>
+	<div style="display: flex; flex-direction: column;">
+		<h1>about</h1>
+		<p>this is the about page.</p>
+	</div>
+</div>
