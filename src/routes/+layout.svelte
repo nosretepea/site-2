62bf9576789c875bd2b909d777fb75468a91c4bc
Ashley Peterson <nosretepea@gmail.com>
2024-01-03 23:58:21 -0500

    Doing some finessing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
-<div style="display: flex; flex-direction: column; align-items: center;">
-	<nav>
-		<a href="/">home</a>
-		<a href="/about">about</a>
-		<a href="/photos">photos</a>
+<div class="main">
+	<nav class="nav">
+		<a class="nav-item" href="/">home</a>
+		<a class="nav-item" href="/about">about</a>
+		<a class="nav-item" href="/photos">photos</a>
 	</nav>
+
+	<slot />
 </div>
 
-<slot />
+<style>
+	.main {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	.nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		margin-top: 1.5rem;
+	}
+
+	.nav-item {
+		padding-right: 0.5rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,64 @@
 <div
 	style="display: flex; flex-direction: column; flex-grow: 1; justify-content: center; text-align: center;"
 >
-	<div style="display: flex; flex-direction: column;">
-		<h1 class="primary">home</h1>
-		<p>this is the home page.</p>
+	<div class="container__outer">
+		<div class="container__inner">
+			<div>
+				<h1 class="title">Ashley Peterson</h1>
+				<p class="subtitle">
+					software engineer, music enthusiast, cat lover, runner, and coffee addict based in Austin,
+					TX
+				</p>
+			</div>
+		</div>
 	</div>
 </div>
 
 <style lang="scss">
 	@import '../styles/colors.scss';
+	@import '../styles/breakpoints.scss';
 
-	.primary {
+	.container__outer {
+		display: flex;
+		flex-direction: row;
+		flex-grow: 1;
+		justify-content: center;
+	}
+
+	.container__inner {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		justify-content: center;
+		text-align: center;
+	}
+
+	.title {
 		color: $l-pink;
+		font-size: 3rem;
+		font-weight: 700;
+		text-transform: lowercase;
+		@include xl {
+			font-size: 4rem;
+		}
+	}
+
+	.subtitle {
+		font-size: 1.125rem;
+		font-weight: 300;
+		padding-top: 1rem;
+		padding-left: 1rem;
+		padding-right: 1rem;
+		@include sm {
+			padding-left: 3rem;
+			padding-right: 3rem;
+		}
+		@include md {
+			font-size: 1rem;
+		}
+		@include lg {
+			padding-left: 4rem;
+			padding-right: 4rem;
+		}
 	}
 </style>
