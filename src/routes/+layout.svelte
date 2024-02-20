f110e58ba06f6ccf8d5fc8a2cfe83d191e42220a
Ashley Peterson <nosretepea@gmail.com>
2024-02-19 22:37:25 -0500

    Removing SCSS that's not being used

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,14 +52,6 @@
 		cursor: pointer;
 	}
 
-	:global(.lightbox > .photo__wrapper img) {
-		width: 100%;
-		height: auto;
-		object-fit: contain;
-		object-position: center;
-		max-height: 80vh;
-	}
-
 	.main {
 		display: flex;
 		flex-direction: column;
